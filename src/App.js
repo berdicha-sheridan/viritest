2ea99f2b6ae1abc26cb1128f7343914ed7a8bdc2
berdicha-sheridan <85641194+berdicha-sheridan@users.noreply.github.com>
2023-08-24 00:07:07 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Work in progress, come back later! :3
+        </p>
+        <p>
+          Awoooooo!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
